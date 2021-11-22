e84e0c250449fe1597743ebda031527e2317457b
Richard Fleming <richard.fleming164@gmail.com>
2021-11-22 03:13:02 +0000

    Task Added, Tests not functioning

---
+++ Packages/ie.itcarlow.2d.platformer_enemy_behaviours/Runtime/FlyingFollower.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingFollower : MonoBehaviour
+{
+    // Enemy's rigidbody
+    Rigidbody2D rb;
+
+    // Enemy Health
+    public float maxHealth = 5.0f;
+    // Enemy movement speed
+    public float speed = 5.0f;
+    // Player GameObject
+    public GameObject player;
+    // Current Health
+    private float health;
+
+    // Enemy Invincibility State
+    // Current Invinibility state
+    public bool invincible = false;
+    // Distance from the player for the enemy to change state
+    public float stateDist = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void damage(float t_damage)
+    {
+        if(!invincible)
+        {
+            health -= t_damage;
+            if(health <= 0.0f)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    public float getHealth()
+    {
+        return health;
+    }
+}
---
+++ Packages/ie.itcarlow.2d.platformer_enemy_behaviours/Tests/FollowerTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class FollowerTest
+    {
+        private GameObject Player;
+        private GameObject Enemy;
+
+        [SetUp]
+        public void SetUp()
+        {
+            SceneManager.LoadScene("TestScene", LoadSceneMode.Single);
+            Player = GameObject.FindGameObjectWithTag("Player");
+            Enemy = GameObject.FindGameObjectWithTag("Follower");
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(Player);
+            Object.Destroy(Enemy);
+            SceneManager.UnloadSceneAsync("TestScene");
+        }
+
+        [UnityTest]
+        public IEnumerator StartPhaseTest()
+        {
+            float initialHealth = Enemy.GetComponent<FlyingFollower>().getHealth();
+            Enemy.GetComponent<FlyingFollower>().invincible = true;
+            Enemy.GetComponent<FlyingFollower>().damage(2);
+            yield return new WaitForSeconds(0.1f);
+            Assert.Equals(Enemy.GetComponent<FlyingFollower>().getHealth(), initialHealth);
+        }
+
+        
+    }
+}
