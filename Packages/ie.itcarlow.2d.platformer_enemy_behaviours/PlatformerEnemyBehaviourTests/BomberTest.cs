b1c31688efc4ff7cbfb5fea089baeb408a70cdd7
Richard Fleming <c00241212@itcarlow.ie>
2022-01-24 08:54:58 +0000

    Feature bomber Jira: LK-13 (#7)
    * Implemented and Tested Port of Bomber Movement Jira: LK-14
    
    * Implemented and Tested Bomber Port Bomb Spawning Jira: LK-20
    
    * Implemented and Tested Bomber Port Bomb Carrying Jira: LK-15
    
    * Implemented and Tested Bomber Port Bomb Drop Jira: LK-16
    
    * Implemented and Tested Bomber Port Bomb Explosion Jira: LK-17
    
    * Implemented and Tested Bomber Port Bomber can take Damage Jira: LK-19
    
    * Implemented and Tested Bomber Port Bomb can take Damage Jira: LK-18

---
+++ Packages/ie.itcarlow.2d.platformer_enemy_behaviours/PlatformerEnemyBehaviourTests/BomberTest.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class BomberTest
+    {
+        private GameObject Player;
+        private GameObject Enemy;
+        private GameObject bomb;
+        private GameObject[] shrapnel;
+
+        [SetUp]
+        public void SetUp()
+        {
+            SceneManager.LoadScene("TestScene", LoadSceneMode.Single);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            SceneManager.UnloadSceneAsync("TestScene");
+        }
+
+        [UnityTest]
+        public IEnumerator Movement()
+        {
+
+            Enemy = GameObject.FindGameObjectWithTag("Bomber");
+            Vector2 initialPos = Enemy.GetComponent<Rigidbody2D>().position;
+            yield return new WaitForSeconds(0.5f);
+            Assert.Less(Enemy.GetComponent<Rigidbody2D>().position.x, initialPos.x);
+        }
+
+        [UnityTest]
+        public IEnumerator BombSpawn()
+        {
+
+            Enemy = GameObject.FindGameObjectWithTag("Bomber");
+            yield return new WaitForSeconds(0.5f);
+            Assert.IsNotNull(Enemy.transform.GetChild(0));
+        }
+
+        [UnityTest]
+        public IEnumerator BombCarry()
+        {
+
+            Enemy = GameObject.FindGameObjectWithTag("Bomber");
+            Vector2 initialPos = Enemy.transform.GetChild(0).position;
+            yield return new WaitForSeconds(0.5f);
+            Assert.Less(Enemy.transform.GetChild(0).position.x, initialPos.x);
+        }
+
+        [UnityTest]
+        public IEnumerator BombDrop()
+        {
+            Enemy = GameObject.FindGameObjectWithTag("Bomber");
+            Vector2 initialPos = Enemy.transform.GetChild(0).position;
+            yield return new WaitForSeconds(2.0f);
+            Assert.Less(Enemy.transform.GetChild(0).position.y, initialPos.y);
+        }
+
+        [UnityTest]
+        public IEnumerator BombExplosion()
+        {
+
+            Enemy = GameObject.FindGameObjectWithTag("Bomber");
+            Enemy.GetComponent<Rigidbody2D>().position = new Vector2(5, 5);
+            yield return new WaitForSeconds(1.5f);
+            shrapnel = GameObject.FindGameObjectsWithTag("Shrapnel");
+            Assert.IsNotNull(shrapnel);
+        }
+
+        [UnityTest]
+        public IEnumerator BomberTakeDamage()
+        {
+            Enemy = GameObject.FindGameObjectWithTag("Bomber");
+            float initialHealth = Enemy.GetComponent<Bomber>().getHealth();
+            Enemy.GetComponent<Bomber>().Damage(2);
+            yield return new WaitForSeconds(0.1f);
+            Assert.Less(Enemy.GetComponent<Bomber>().getHealth(), initialHealth);
+        }
+
+        [UnityTest]
+        public IEnumerator BombTakeDamage()
+        {
+            Enemy = GameObject.FindGameObjectWithTag("Bomber");
+            float initialHealth = Enemy.GetComponentInChildren<Bomb>().getHealth();
+            Enemy.GetComponentInChildren<Bomb>().Damage(0.5f);
+            yield return new WaitForSeconds(0.1f);
+            Assert.Less(Enemy.GetComponentInChildren<Bomb>().getHealth(), initialHealth);
+        }
+    }
+}
---
+++ Packages/ie.itcarlow.2d.platformer_enemy_behaviours/Runtime/Bomber.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomber : MonoBehaviour
+{
+    // Enemy's rigidbody
+    Rigidbody2D rb;
+
+    // Enemy Health
+    public float maxHealth = 5.0f;
+    // Enemy movement speed
+    public float speed = 5.0f;
+    // Player GameObject
+    public GameObject player;
+    // Bomb GameObject
+    private GameObject bomb;
+    // Current Health
+    private float health;
+
+    // Bombing Range
+    public float range = 5;
+
+    private bool armed = true;
+
+    // Passed in Bomb Object
+    public GameObject bombPassed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        health = maxHealth;
+        // Movement
+        rb.velocity = new Vector2(-speed, rb.velocity.y);
+        bomb = Instantiate(bombPassed, new Vector3(rb.position.x, rb.position.y - 1.5f, 0), Quaternion.identity, transform);
+        if(!bomb.GetComponent<Bomb>().enabled)
+        {
+            bomb.GetComponent<Bomb>().enabled = true;
+        }
+        if (!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (armed)
+        {
+            if(player && bomb)
+            {
+                if (Vector2.Distance(rb.position, new Vector2(player.transform.position.x, player.transform.position.y)) < range && !bomb.GetComponent<Bomb>().dropped)
+                {
+                    bomb.GetComponent<Bomb>().dropped = true;
+                    armed = false;
+                }
+            }
+            
+        }
+    }
+
+    public void Damage(float damage)
+    {
+        if (health - damage > 0)
+        {
+            health -= damage;
+        }
+        else
+        {
+            health = 0;
+            Destroy(this.gameObject);
+        }
+    }
+
+    public float getHealth()
+    {
+        return health;
+    }
+}
